c549d77470d5678e69380508f9bddc3d1084666b
gabriellenicole <gabrielle.nicole2003@gmail.com>
2024-10-08 12:02:44 +0800

    feat: remove hero padding

--- src/components/section/hero.tsx
+++ src/components/section/hero.tsx
@@ -2,7 +2,7 @@ import { FlippingGaby } from '../ui/flipping-gaby'
 import { BentoGrid } from '../ui/bento-grid'
 export default function Hero() {
   return (
-    <div className="flex w-full max-w-xl flex-col gap-20 px-10 py-10 xl:h-screen xl:max-w-none xl:flex-row xl:items-center xl:justify-start xl:px-20">
+    <div className="flex w-full max-w-xl flex-col gap-20 px-10 py-10 xl:h-screen xl:max-w-none xl:flex-row xl:items-center xl:justify-start 2xl:px-0">
       <div className="flex h-full flex-col items-start gap-5 pt-20 text-left xl:w-1/3 xl:justify-start">
         <FlippingGaby />
         <h1 className="mt-5 text-4xl font-semibold tracking-wide xl:text-5xl">
