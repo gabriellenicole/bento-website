78ffc84ae46e59d987d66fe8715076c348c62560
gabriellenicole <gabrielle.nicole2003@gmail.com>
2024-10-08 18:03:48 +0800

    feat: auto flip and bento grid

--- src/components/ui/bento-grid.tsx
+++ src/components/ui/bento-grid.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import locationMap from '@/assets/singapore.png'
 import linkedinIcon from '@/assets/linkedin.png'
 import githubIcon from '@/assets/github.png'
@@ -11,22 +12,41 @@ import spotifyAlbum4 from '@/assets/spotify_nicole.jpeg'
 import { MBTIProfileBox } from './mbti'
 import { Globe } from 'lucide-react'
 import { SmileyIcon } from './icons'
+import { cn } from '@/lib/utils'
+
+interface BentoBoxProps {
+  children: React.ReactNode
+  className?: string
+}
+
+const BentoBox: React.FC<BentoBoxProps> = ({ children, className }) => {
+  return (
+    <div
+      className={cn(
+        'rounded-xl border border-gray-200 transition-all duration-100 hover:scale-[1.02]',
+        className,
+      )}
+    >
+      {children}
+    </div>
+  )
+}
 
 export const BentoGrid = () => {
   return (
     <div className="w-full max-w-2xl xl:w-[850px] xl:max-w-none">
       <div className="grid grid-cols-2 gap-4 xl:grid-cols-4">
         {/* Pink Box */}
-        <div className="row-span-2 flex flex-col items-center justify-between rounded-xl bg-[#F51672] py-8">
+        <BentoBox className="row-span-2 flex flex-col items-center justify-between bg-[#F51672] py-8">
           <div className="h-5 w-5 rounded-full bg-white"></div>
           <div className="flex flex-col items-center gap-2 text-white">
             <SmileyIcon className="h-20 w-20 stroke-none" />
             <div className="text-4xl font-bold tracking-wide">GABY</div>
           </div>
-        </div>
+        </BentoBox>
 
         {/* LinkedIn Box */}
-        <div className="bg-white-200 flex aspect-square flex-col justify-between rounded-xl border border-gray-200 bg-white p-4 sm:p-7 xl:p-5">
+        <BentoBox className="flex aspect-square flex-col justify-between sm:p-7 xl:p-5">
           <div className="flex flex-col">
             <img src={linkedinIcon} alt="LinkedIn" className="mb-2 h-8 w-8" />
             <div className="font-semibold text-gray-800">linkedin.com</div>
@@ -35,20 +55,20 @@ export const BentoGrid = () => {
           <button className="-mr-1 ml-auto rounded-full border border-sky-600 bg-white px-3 py-1 text-sm font-semibold text-sky-600 transition-colors hover:bg-blue-50">
             Connect
           </button>
-        </div>
+        </BentoBox>
 
         {/* Location */}
-        <div className="relative col-span-2 row-span-2 aspect-square rounded-xl">
+        <BentoBox className="relative col-span-2 row-span-2 aspect-square p-0">
           <img src={locationMap} alt="Location" className="h-full w-full rounded-xl object-cover" />
           <span className="absolute left-1/2 top-1/2 h-4 w-4 animate-[ping_1.5s_ease-in-out_infinite] rounded-full bg-blue-400 opacity-75"></span>
           <span className="absolute left-1/2 top-1/2 h-4 w-4 rounded-full border-2 bg-blue-500"></span>
           <div className="absolute bottom-4 left-6 rounded-lg border border-gray-300 bg-white bg-opacity-75 px-2 py-1 text-sm text-neutral-800 xl:bottom-2 xl:left-4">
             📍 Based in Singapore
           </div>
-        </div>
+        </BentoBox>
 
         {/* Github */}
-        <div className="bg-white-200 col-start-2 row-start-2 flex aspect-square flex-col justify-between rounded-xl border border-gray-200 bg-white p-4 sm:p-7 xl:p-5">
+        <BentoBox className="bg-white-200 col-start-2 row-start-2 flex aspect-square flex-col justify-between rounded-xl border border-gray-200 bg-white p-4 sm:p-7 xl:p-5">
           <div className="flex flex-col">
             <img src={githubIcon} alt="Github" className="mb-2 h-8 w-8" />
             <div className="font-semibold text-gray-800">github.com</div>
@@ -57,32 +77,32 @@ export const BentoGrid = () => {
           <button className="-mr-1 ml-auto rounded-full border border-gray-600 bg-white px-3 py-1 text-sm font-semibold text-gray-600 transition-colors hover:bg-gray-50">
             Follow
           </button>
-        </div>
+        </BentoBox>
 
-        <div className="col-span-2 flex aspect-[2/1] flex-col justify-between rounded-xl bg-[#74F4F3] p-7 xl:aspect-auto">
+        <BentoBox className="col-span-2 flex aspect-[2/1] flex-col justify-between rounded-xl bg-[#74F4F3] p-7 xl:aspect-auto">
           <Globe className="ml-auto mr-0 h-10 w-10 text-blue-950" size={24} />
           <p className="text-xl font-bold leading-tight text-blue-950">
             born and raised in
             <br />
             <span className="text-3xl text-blue-950">indonesia 🇮🇩</span>
           </p>
-        </div>
+        </BentoBox>
 
         {/* Cafe */}
-        <div className="aspect-square rounded-xl bg-gray-200">
+        <BentoBox className="aspect-square rounded-xl bg-gray-200">
           <img src={cafeImage} alt="Cafe" className="h-full w-full rounded-xl object-cover" />
-        </div>
+        </BentoBox>
 
         {/* Sunset */}
-        <div className="row-span-2 rounded-xl bg-purple-500">
+        <BentoBox className="row-span-2 rounded-xl bg-purple-500">
           <MBTIProfileBox />
-        </div>
-        <div className="aspect-square rounded-xl bg-gray-200">
+        </BentoBox>
+        <BentoBox className="aspect-square rounded-xl bg-gray-200">
           <img src={sunsetImage} alt="Sunset" className="h-full w-full rounded-xl object-cover" />
-        </div>
+        </BentoBox>
 
         {/* Spotify */}
-        <div className="col-span-2 flex aspect-[2/1] items-center justify-between rounded-xl border border-gray-200 bg-white p-5 sm:p-8 xl:aspect-auto xl:px-5 xl:py-4">
+        <BentoBox className="col-span-2 flex aspect-[2/1] items-center justify-between rounded-xl border border-gray-200 bg-white p-5 sm:p-8 xl:aspect-auto xl:px-5 xl:py-4">
           <div className="flex h-full flex-col items-start justify-between">
             <div className="flex flex-col items-start">
               <img src={spotifyIcon} alt="Spotify" className="mb-1 h-10 w-10" />
@@ -116,7 +136,7 @@ export const BentoGrid = () => {
               className="h-20 w-20 rounded-xl xl:h-16 xl:w-16"
             />
           </div>
-        </div>
+        </BentoBox>
       </div>
     </div>
   )
--- src/components/ui/flipping-gaby.tsx
+++ src/components/ui/flipping-gaby.tsx
@@ -1,25 +1,52 @@
+import { useEffect, useState } from 'react'
+import { motion } from 'framer-motion'
 import profileFront from '../../assets/profile_front.jpeg'
 import profileBack from '../../assets/profile_back.jpeg'
 
 export const FlippingGaby = () => {
+  const [isFlipped, setIsFlipped] = useState(false)
+  const [autoFlipComplete, setAutoFlipComplete] = useState(false)
+
+  useEffect(() => {
+    // Auto flip on component mount
+    setIsFlipped(true)
+
+    // Set auto flip to complete after 3 seconds
+    const timer = setTimeout(() => {
+      setIsFlipped(false)
+      setAutoFlipComplete(true)
+    }, 1000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
-    <div className="flip-card h-32 w-32 xl:h-52 xl:w-52">
-      <div className="flip-card-inner">
+    <div
+      className="flip-card h-32 w-32 xl:h-52 xl:w-52"
+      onMouseEnter={() => autoFlipComplete && setIsFlipped(true)}
+      onMouseLeave={() => autoFlipComplete && setIsFlipped(false)}
+    >
+      <motion.div
+        className="flip-card-inner"
+        initial={false}
+        animate={{ rotateY: isFlipped ? 180 : 0 }}
+        transition={{ duration: 0.3, ease: 'easeInOut' }}
+      >
         <div className="flip-card-front">
           <img
             src={profileFront}
             alt="Profile Front"
-            className="-full h-full w-full rounded-full object-cover"
+            className="h-full w-full rounded-full object-cover"
           />
         </div>
-        <div className="flip-card-back">
+        <motion.div className="flip-card-back" style={{ rotateY: 180 }}>
           <img
             src={profileBack}
             alt="Profile Back"
             className="h-full w-full rounded-full object-cover"
           />
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   )
 }
